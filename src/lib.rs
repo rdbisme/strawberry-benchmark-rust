5c9a9385b8e96f5f8e13b93b5f8b98021bc23f76
Ruben Di Battista <rubendibattista@gmail.com>
2022-05-06 19:35:29 +0200

    :fire: refactor: Remove comments

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,3 @@
-// use pyo3::prelude::*;
-//
-// /// Formats the sum of two numbers as string.
-// #[pyfunction]
-// fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-//     Ok((a + b).to_string())
-// }
-//
-// /// A Python module implemented in Rust.
-// #[pymodule]
-// fn rust_bench_strawberry(_py: Python, m: &PyModule) -> PyResult<()> {
-//     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
-//     Ok(())
-// }
-
 use juniper::{graphql_object, GraphQLObject};
 
 #[derive(GraphQLObject, Clone)]
